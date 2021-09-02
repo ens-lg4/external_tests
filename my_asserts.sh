086738193f81ddc243f749ca5dde0c0409bed3c2
Leo Gordon <lg4@krai.ai>
2021-09-02 20:00:59 +0100

    see if we can parse it

--- my_asserts.sh
+++ my_asserts.sh
@@ -8,5 +8,14 @@ assert_end built_in_command
 assert 'hello_world.py' 'Hello, world!'
 assert_end python_script
 
+if [ `uname` = 'Darwin' ]; then
+    MD5CMD="md5 -r"
+else
+    MD5CMD="md5sum"
+fi
+
+assert '$MD5CMD hello_world.txt | cut -f 1 -d " "' 'a7966bf58e23583c9a5a4059383ff850'
+assert_end md5sum_and_parsing
+
 echo "Assert tests done"
 

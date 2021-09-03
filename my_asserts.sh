edfb4426a1dc36260c2a16c96b71f359d5b53017
Leo Gordon <lg4@krai.ai>
2021-09-03 21:00:56 +0100

    what does the output of /usr/bin/md5sum look like on windows?

--- my_asserts.sh
+++ my_asserts.sh
@@ -2,14 +2,6 @@
 
 source assert.sh
 
-echo "Operating system:"
-uname -s
-echo "Which uname:"
-which uname
-echo "Which md5sum:"
-which md5sum
-echo "-----------------"
-
 assert 'echo "Hello, world!"' 'Hello, world!'
 assert_end built_in_command
 
@@ -21,6 +13,10 @@ if [ `uname` = 'Darwin' ]; then
 else
     MD5CMD="md5sum"
 fi
+
+echo "-----------------"
+$MD5CMD hello_world.txt
+echo "================="
 
 assert '$MD5CMD hello_world.txt | cut -f 1 -d " "' 'a7966bf58e23583c9a5a4059383ff850'
 assert_end md5sum_and_parsing
